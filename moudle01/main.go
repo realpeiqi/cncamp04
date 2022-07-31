308baa260d6ffc1b35e61551299efed5dcbe2b89
realpeiqi <dynine@126.com>
2022-07-31 16:21:03 +0000

    update  main.go in moudle01

--- moudle01/main.go
+++ moudle01/main.go
@@ -1,8 +1,7 @@
-package module01
+package main
 
 import (
 	"fmt"
-	"time"
 )
 
 // 编写一个小程序：
@@ -24,30 +23,8 @@ func Exercise1() {
 	fmt.Println(inputList)
 }
 
-// 基于 Channel 编写一个简单的单线程生产者消费者模型：
-// 队列：
-// 队列长度 10，队列元素类型为 int
-// 生产者：
-// 每 1 秒往队列中放入一个类型为 int 的元素，队列满时生产者可以阻塞
-// 消费者：
-// 每一秒从队列中获取一个元素并打印，队列为空时消费者阻塞
+func main(){
 
-func Exercise2() {
-	ch := make(chan int)
+	Exercise1()
 
-	go func() {
-		for i := 0; i < 10; i++ {
-			ticker := time.NewTicker(time.Second)
-			defer ticker.Stop()
-			<-ticker.C
-			ch <- i
-			fmt.Printf("put %d into channel\n", i)
-		}
-		defer close(ch)
-	}()
-
-	for v := range ch {
-		time.Sleep(time.Second)
-		fmt.Printf("get %d out of channel\n", v)
-	}
 }
